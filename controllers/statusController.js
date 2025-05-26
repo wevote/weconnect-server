a5c545aea04f6bd99edefee5b14d4b836dcba947
stevepodell <steve@podell.com>
2025-05-26 14:50:22 -0700

    [WV-1139] Final cleanup for fast load

--- controllers/statusController.js
+++ controllers/statusController.js
@@ -1,6 +1,7 @@
 const util = require('util');
 const fs = require('node:fs');
 const exec = util.promisify(require('child_process').exec);
+const { DateTime } = require('luxon');
 
 exports.getStatus = async (req, res) => {
   const ret = {};
@@ -11,6 +12,19 @@ exports.getStatus = async (req, res) => {
     ret.git_commit_hash = `https://github.com/wevote/weconnect-server/commit/${hash}`;
   } catch (error) {
     ret.uname = 'uname error';
+  }
+
+  try {
+    const response = await fetch(ret.git_commit_hash);
+    const text = await response.text();
+    const dateStringResults = text.match(/"committedDate":"(.*?)"/);
+    console.log(dateStringResults[1]);
+    const date = new DateTime(dateStringResults[1]);
+    ret.git_committed_date = date.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+    const pr = text.match(/"Merge pull request (.*?)wevote/);
+    ret.PR = pr[1].slice(0, -2);
+  } catch (error) {
+    console.log(error);
   }
 
   try {
