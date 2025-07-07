10fa67c3d3fb24f89272d77e3d51a07eb7a63fc1
Dale John McGrew <Dale.McGrew@WeVote.US>
2025-07-07 07:31:41 -0700

    Adding fix to keep deployment pipeline running. (i.e. adding new /health page)

--- weconnect-server.js
+++ weconnect-server.js
@@ -37,7 +37,7 @@ const secureTransfer = (process.env.BASE_URL.startsWith('https'));
 
 const ACCESS_PATHS_ALLOWED_PRE_AUTH = ['/', '/favicon.ico', '/health', '/healthapis/v1/versions', '/we-vote-logo-wordmark-vertical-color-on-white-256x256.png'];
 const ACCESS_APIS_ALLOWED_PRE_AUTH = [
-  'answer-list-save', 'get-auth', 'login', 'logout', 'person-id-retrieve-by-email',
+  'answer-list-save', 'get-auth', 'login', 'logout', 'person-id-retrieve-by-email', 'person-retrieve-by-email',
   'question-list-retrieve', 'questionnaire-list-retrieve', 'save-password', 'send-email-code', 'signup',
   'task-definition-list-retrieve', 'task-group-list-retrieve', 'task-group-team-link-list-retrieve',
   'verify-email-code', 'versions',
